d8b0c5509841fb328a7147edef358bfb7a26ecbe
tranduythanh <coachtranduythanh@gmail.com>
2024-01-24 12:10:12 +0700

    done part2indexC_inner

--- testcases/test_part_index.py
+++ testcases/test_part_index.py
@@ -77,7 +77,45 @@ class TestPart2IndexBInner(unittest.TestCase):
         expected = 'S[8,9]'
         self.assertEqual(part2indexB_inner(lam, k, n), expected)        
 
-    
+
+class TestPart2IndexCInner(unittest.TestCase):    
+    def test_case_1(self):
+        lam = [3,2,1]
+        k = 2
+        n = 5
+        expected = 'S[5,7,9]'
+        self.assertEqual(part2indexC_inner(lam, k, n), expected)
+        
+    def test_case_2(self):
+        lam = [6,5,4]
+        k = 3
+        n = 7
+        expected = 'S[5,6,7,14]'
+        self.assertEqual(part2indexC_inner(lam, k, n), expected)
+        
+    def test_case_3(self):
+        lam = [4,3,2]
+        k = 1
+        n = 6
+        expected = 'S[4,5,6,11,12]'
+        self.assertEqual(part2indexC_inner(lam, k, n), expected)
+        
+    def test_case_4(self):
+        lam = [1, 1, 1]
+        k = 0
+        n = 3
+        expected = 'S[3,3,4]'
+        self.assertEqual(part2indexC_inner(lam, k, n), expected)
+        
+    def test_case_5(self):
+        lam = [0, 0, 0]
+        k = 2
+        n = 4
+        expected = 'S[7,8]'
+        self.assertEqual(part2indexC_inner(lam, k, n), expected)    
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
