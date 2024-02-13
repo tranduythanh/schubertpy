8440307eb73e29a80e4b581691d7e8bbb4f94630
tranduythanh <coachtranduythanh@gmail.com>
2024-02-13 23:10:09 +0700

    Bump version: 0.3.9 → 0.3.10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_requirements():
     
 setup(
     name='schubertpy',
-    version='0.3.9',
+    version='0.3.10',
     packages=find_packages(),
     description='A brief description of your package',
     long_description=open('README.md').read(),
