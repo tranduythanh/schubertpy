6c5cbd64bb8796c5c77c62be82393a2ada4399f6
tranduythanh <coachtranduythanh@gmail.com>
2024-02-13 23:13:04 +0700

    Bump version: 0.3.10 → 0.3.11

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ def read_requirements():
     with open('requirements.txt') as f:
         return [line.strip() for line in f if line.strip()]
     
+
+# Read the contents of your README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='schubertpy',
-    version='0.3.10',
+    version='0.3.11',
     packages=find_packages(),
     description='A brief description of your package',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     python_requires='>=3.6',
     author='Trần Duy Thanh',
     author_email='fbtranduythanh@gmail.com',
