cea4f1b287db286e933550885bd3da1880f1c081
tranduythanh <coachtranduythanh@gmail.com>
2024-02-13 20:21:08 +0700

    setup and publish library

--- Makefile
+++ Makefile
@@ -7,3 +7,11 @@ coverage:
 	python3 -m coverage run -m unittest testcases/*.py
 	python -m coverage report
 	python -m coverage html
+
+publish:
+	rm -rf dist
+	rm -rf build
+	rm -rf schulze.egg-info
+	pipreqs --force ./
+	python3 setup.py sdist bdist_wheel
+	python3 -m twine upload dist/*
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# Function to read the contents of the requirements.txt file
+def read_requirements():
+    with open('requirements.txt') as f:
+        return [line.strip() for line in f if line.strip()]
+    
+setup(
+    name='schubertpy',
+    version='0.2',
+    packages=find_packages(),
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    python_requires='>=3.6',
+    author='Trần Duy Thanh',
+    author_email='fbtranduythanh@gmail.com',
+    url='https://github.com/tranduythanh/schubertpy',
+    install_requires=read_requirements(),
+)
